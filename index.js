397cc7e5b5bc3de67c2b02f81bd15d28595f01a9
iidastudio <philip.w.shotaro@gmail.com>
2021-10-29 03:28:37 +0900

    classlaization and modularization

--- index.js
+++ index.js
@@ -1,47 +1,85 @@
-import { merge } from 'lodash-es';
-import fs from 'fs';
-import path from 'path';
-const targetDir = './json';
-const baseObj = JSON.parse(fs.readFileSync('./json/base.json', 'utf8'));;
-let resultObj = {};
-let directories = [];
+const lodash = require('lodash');
+const fs = require('fs');
+const path = require('path');
 
-// check functions
-const isJsonFile = (item, itemPath) => item && fs.statSync(itemPath).isFile() && item.match(/^_.*\.(json)$/);
+module.exports = class MergeThemeJson {
+  
+  constructor(baseFile, targetDirArray, output) {
+    this.baseFile = baseFile;
+    this.targetDirArray = targetDirArray;
+    this.output = output;
+    this.jsonFiles = [];
+    this.directories = [];
+    this.resultObj = {};
+  }
+  
+  // get file and directory names
+  getJsonFiles = (dir) => {
+    if (fs.existsSync(dir)) {
+      const filenames = fs.readdirSync(dir);
+      
+      for (let i = 0; i < filenames.length; i++) {
+        const filename = filenames[i];
+        const fullPath = path.join(dir, filename);
+        const isJsonFile = filename && fs.statSync(fullPath).isFile() && filename.match(/^_.*\.(json)$/);
+        
+        // case of file
+        if(isJsonFile){
+          this.jsonFiles.push(fullPath);
 
-// merge json objects
-const mergeJson = (targetObj) => {
-  resultObj = merge(baseObj, targetObj);
-}
-
-// get file and directory names
-const getJsonFiles = (dir) => {
-  const filenames = fs.readdirSync(dir);
-
-  for (let i = 0; i < filenames.length; i++) {
-    const filename = filenames[i];
-    const fullPath = path.join(dir, filename);
-
-    // case of file
-    if(isJsonFile(filename, fullPath)){
-      const loadedObj = JSON.parse(fs.readFileSync(fullPath, 'utf8'));
-      mergeJson(loadedObj);
-
-    // case of directory
-    }else if(fs.statSync(fullPath).isDirectory()) {
-      directories.push(fullPath);
+        // case of directory
+        }else if(fs.statSync(fullPath).isDirectory()) {
+          this.directories.push(fullPath);
+        }
+      }
+    } else {
+      console.error('This directory is not found. %j', dir);
     }
   }
-}
 
-getJsonFiles(targetDir);
-// get child in dir
-if (directories.length) {
-  for (let i = 0; i <  directories.length; i++) {
-    const dirname = directories[i];
-    getJsonFiles(dirname);
+  // merge json objects
+  mergeJson = (jsonFiles) => {
+    if (fs.existsSync(this.baseFile)) {
+      const baseObj = JSON.parse(fs.readFileSync(this.baseFile, 'utf8'));
+      for (let i = 0; i < jsonFiles.length; i++) {
+        const jsonFile = jsonFiles[i];
+        const targetObj = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+        this.resultObj = lodash.merge(baseObj, targetObj);
+      }
+    } else {
+      console.error('This file is not found. %j', this.baseFile);
+    }
   }
-}
 
-const result = JSON.stringify(resultObj, null, 2);
-fs.writeFileSync('theme.json', result);
+  // export json
+  exportJson = (resultObj) => {
+    const result = JSON.stringify(resultObj, null, 2);
+    if(!this.output.match(/.*\.(json)$/)) {
+      this.output = this.output + '.json';
+    }
+    fs.writeFileSync(this.output, result);
+  }
+
+  // use all functions to generate merged json
+  run = () => {
+    let isTargetDir;
+    for (let i = 0; i < this.targetDirArray.length; i++) {
+      const targetDir = this.targetDirArray[i];
+      this.getJsonFiles(targetDir);
+      if (fs.existsSync(targetDir)) {
+        isTargetDir = true;
+      }
+    }
+    if (this.directories.length) {
+      for (let i = 0; i < this.directories.length; i++) {
+        const childDir = this.directories[i];
+        this.getJsonFiles(childDir);
+      }
+    }
+    if (isTargetDir === true) {
+      this.mergeJson(this.jsonFiles);
+      this.exportJson(this.resultObj);
+    }
+  }
+
+}
